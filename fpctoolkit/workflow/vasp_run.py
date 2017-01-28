ddecd641906559299a200bd27a1b965ffce8bfed
Thomas Angsten <tangst10@gmail.com>
2017-01-27 16:48:17 -0800

    triv

--- fpctoolkit/workflow/vasp_run.py
+++ fpctoolkit/workflow/vasp_run.py
@@ -251,7 +251,7 @@ class VaspRun(object):
 		head_string = "==> "
 		file_separator = 30*"-"
 		output_string = ""
-		output_string += "\n" + 30*"-" + "VaspRun View: Job ID is " + str(self.job_id_string) + 30*"-" + "\n"
+		output_string += "\n\n" + 30*"-" + "VaspRun View: Job ID is " + str(self.job_id_string) + 30*"-" + "\n"
 		output_string += head_string + "Path: " + self.path + "\n"
 
 		for file_name in files_to_view:
@@ -272,7 +272,7 @@ class VaspRun(object):
 
 			output_string += head_string + actual_file_name + ":\n" + file_separator + "\n" + file_string + file_separator + "\n"
 
-		output_string += "\n" + 30*"-" + "End VaspRun View for Job ID " + str(self.job_id_string) + 30*"-" + "\n\n"
+		output_string += "\n" + 30*"-" + "End VaspRun View for Job ID " + str(self.job_id_string) + 30*"-" + "\n\n\n"
 
 		print output_string,
 
