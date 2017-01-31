322f6e3b9b248a9e3df7581660b0a70ec017d43f
Thomas Angsten <tangst10@gmail.com>
2017-01-30 18:25:26 -0800

    vasp relaxation class updated

---
+++ fpctoolkit/workflow/parameter_list.py
@@ -0,0 +1,29 @@
+
+
+
+
+class ParameterList(object):
+	"""
+	A list with special access features tailored to run parameter lists.
+
+	Useful for using with vasp run sets as the internal parameter
+	change list.
+	"""
+
+	def __init__(parameter_list):
+		self._parameter_list = parameter_list
+
+	def __getitem__(index):
+		if (not isinstance(index, int)) or index < 0:
+			raise KeyError()
+		else:
+			if index < len(self._parameter_list):
+				return self._parameter_list[index]
+			elif len(self._parameter_list) > 0:
+				return self._parameter_list[-1]
+			else:
+				raise Exception("No elements in parameter list")
+
+
+
+
--- fpctoolkit/workflow/vasp_relaxation.py
+++ fpctoolkit/workflow/vasp_relaxation.py
@@ -2,7 +2,7 @@
 
 from fpctoolkit.workflow.vasp_run import VaspRun
 from fpctoolkit.workflow.vasp_run_set import VaspRunSet
-from fpctoolkit.workflow.flexible_parameter_list import FlexibleParameterList
+from fpctoolkit.workflow.flexible_parameter_list import ParameterList
 
 class VaspRelaxation(VaspRunSet):
 	"""
@@ -18,20 +18,23 @@ class VaspRelaxation(VaspRunSet):
 	4, then four external relaxations (isif = 3) will be performed before running a static.
 
 	If external_relaxation_count == 0, then only a static is performed
+
+	To have wavecars copied and used in the relaxation chain to increase performance, include WAVECAR in the
+	incar modifications. 
 	"""
 
 
-	def __init__(self, path, initial_structure=None, external_relaxation_count=2, use_wavecars=True, incar_modifications_list=None, kpoint_subdivisions_lists=None, kpoint_schemes=None, verbose=True):
+	def __init__(self, path, initial_structure=None, external_relaxation_count=2, incar_modifications_list=None, kpoint_subdivisions_lists=None, kpoint_schemes_list=None, verbose=True):
 		
 		self.path = Path.clean(path)
 		self.verbose = verbose
-		self.run_count = 0
+		self.run_count = 0 #how many runs have been initiated
 
 		if external_relaxation_count < 0:
 			raise Exception("Must have one or more external relaxations")
 
 		self.external_relaxation_count = external_relaxation_count
-		self.use_wavecars = use_wavecars
+		self.kpoint_schemes = ParameterList(kpoint_schemes_list)
 		self.incar_modifiers
 
 		self.vasp_run_list = []
@@ -45,28 +48,50 @@ class VaspRelaxation(VaspRunSet):
 		else: #self.path is either an empty directory or does not exist
 			Path.make(self.path)
 
-	def start(self):
-		run_path = Path.join(encut_convergence_set_path, str(encut))
+	def create_next_run(self):
+		run_path = self.get_next_run_path()
 
-		kpoints = Kpoints(scheme_string=base_kpoints_scheme, subdivisions_list=base_kpoints_subdivisions_list)
+		kpoints = Kpoints(scheme_string=self.kpoint_schemes[self.run_count], subdivisions_list=base_kpoints_subdivisions_list)
 		incar = IncarMaker.get_static_incar({'ediff':base_ediff, 'encut':encut})
 		input_set = VaspInputSet(base_structure, kpoints, incar)
 
-		vasp_run = VaspRun(run_path, input_set=input_set)
-
+		vasp_run = VaspRun(run_path, input_set=input_set, verbose=self.verbose)
+		self.run_count += 1
 
 	def update(self):
 
-		if len(self.vasp_run_list) == 0:
-			self.start()
+		if self.run_count == 0 or self.get_current_vasp_run.complete:
+			self.create_next_run()
+
+
+		#if wavecar exists, make sure it is copied over
 
 	def load(self):
 		pass
 
-	def get_next_path(self):
+	def get_next_run_path(self):
+		return self.get_extended_path(self.get_next_run_path_basename)
+
+	def get_next_run_path_basename(self):
 		"""
-		Returns relax_1 if no relaxes, returns relax_4 if three other relaxes exist,
-		returns static if self.run_count == 
+		Returns relax_1 if no relaxes, returns relax_4 if three other relaxes exist, 
+		returns static if self.run_count == self.external_relaxation_count
+		"""
+
+		if self.run_count == self.external_relaxation_count:
+			return 'static'
+		else:
+			return 'relax_' + str(self.run_count + 1)
+
+	def get_current_run_path(self):
+		return self.get_extended_path(self.get_current_run_path_basename)
+
+	def get_current_run_path_basename(self):
+		if self.run_count == self.external_relaxation_count:
+			return 'static'
+		else:
+			return 'relax_' + str(self.run_count)
+
 
 	def get_extended_path(self, relative_path):
 		return Path.join(self.path, relative_path)
