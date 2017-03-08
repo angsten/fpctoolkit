a8fdcd2e8f1db37014e7037323342a7e3a8ca3bc
Thomas Angsten <tangst10@gmail.com>
2017-03-08 10:04:50 -0800

    Trivial debug change

--- fpctoolkit/workflow/vasp_relaxation.py
+++ fpctoolkit/workflow/vasp_relaxation.py
@@ -58,6 +58,7 @@ class VaspRelaxation(VaspRunSet):
 		self.path = Path.clean(path)
 
 		if not input_dictionary:
+			print "LOADING RELAXATION"
 			self.load()
 		else:
 			self.verbose = verbose
@@ -218,6 +219,7 @@ class VaspRelaxation(VaspRunSet):
 		if self.submission_node_count_list:
 			input_set.set_node_count(self.submission_node_count_list[self.run_count])
 
+		print "INPUT SET LOOKS LIKE:", input_set
 
 		vasp_run = VaspRun(run_path, input_set=input_set, verbose=self.verbose, wavecar_path=self.get_wavecar_path())
 
