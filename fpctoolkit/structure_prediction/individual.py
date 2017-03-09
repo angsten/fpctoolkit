1c6b28a57d2c05020728b61a5ad95057aae39c89
Thomas Angsten <tangst10@gmail.com>
2017-03-09 12:59:56 -0800

    Trivial debug change

--- fpctoolkit/structure_prediction/individual.py
+++ fpctoolkit/structure_prediction/individual.py
@@ -19,6 +19,7 @@ class Individual(object):
 
 			self.load()
 		else:
+			self.path = calculation_set.path
 			self.calculation_set = calculation_set
 
 			self.structure_creation_id_string = structure_creation_id_string if structure_creation_id_string else self.get_structure_creation_id_string()
@@ -51,7 +52,7 @@ class Individual(object):
 		self.calculation_set.update()
 
 	def get_extended_path(self, relative_path):
-		return Path.join(self.calculation_set.path, relative_path)
+		return Path.join(self.path, relative_path)
 
 	def get_structure_creation_id_file_path(self):
 		return self.get_extended_path('.creation_id')
