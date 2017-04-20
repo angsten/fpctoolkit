099d910825d673f8ba80dac533a1b20661a85201
Thomas Angsten <tangst10@gmail.com>
2017-04-20 16:41:27 -0700

    Trivial debug change

--- examples/phonon_normal_mode_energy_tester.py
+++ examples/phonon_normal_mode_energy_tester.py
@@ -50,11 +50,16 @@ q_points_list = [(0.0, 0.0, 0.0), (0.5, 0.0, 0.0), (0.0, 0.5, 0.0), (0.0, 0.0, 0
 pbs = phonopy_utility.get_phonon_band_structure_instance(phonopy_instance=phonon, q_points_list=q_points_list)
 
 
+coordinate_index = 180
+
+coordinate_path = Path.join(base_path, "coordinate_index_" + str(coordinate_index))
+	
+Path.make(coordinate_path)
 
 for i in range(4):
+	relaxatoin_path = Path.join(coordinate_path, str(i))
 	ps = PhononStructure(primitive_cell_structure=pbs.primitive_cell_structure, phonon_band_structure=pbs, supercell_dimensions_list=phonopy_inputs_dictionary['supercell_dimensions'])
 
-	coordinate_index = 180
 	ps.normal_coordinates_list[coordinate_index].coefficient = i*0.1
 
 	dist_struct = ps.get_distorted_supercell_structure()
