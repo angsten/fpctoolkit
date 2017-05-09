637da3442c53264bb2929f9802bcd9524f5f1e4f
Thomas Angsten <tangst10@gmail.com>
2017-05-09 12:47:32 -0700

    Trivial debug change

--- examples/derivative_evaluation.py
+++ examples/derivative_evaluation.py
@@ -5,6 +5,8 @@ from fpctoolkit.structure_prediction.taylor_expansion.variable import Variable
 from fpctoolkit.structure_prediction.taylor_expansion.expansion_term import ExpansionTerm
 from fpctoolkit.structure_prediction.taylor_expansion.taylor_expansion import TaylorExpansion
 from fpctoolkit.structure_prediction.taylor_expansion.derivative_evaluator import DerivativeEvaluator
+from fpctoolkit.workflow.vasp_relaxation import VaspRelaxation
+from fpctoolkit.util.path import Path
 
 
 
