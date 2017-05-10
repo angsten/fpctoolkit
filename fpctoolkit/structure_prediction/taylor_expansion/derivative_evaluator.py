878eeb86d869379e136b5f2e9fcf852383643a90
Thomas Angsten <tangst10@gmail.com>
2017-05-10 15:42:33 -0700

    Trivial debug change

--- fpctoolkit/structure_prediction/taylor_expansion/derivative_evaluator.py
+++ fpctoolkit/structure_prediction/taylor_expansion/derivative_evaluator.py
@@ -1,6 +1,7 @@
 #from fpctoolkit.structure_prediction.taylor_expansion.derivative_evaluator import DerivativeEvaluator
 
 import copy
+import numpy as np
 
 from fpctoolkit.workflow.vasp_static_run_set import VaspStaticRunSet
 from fpctoolkit.util.path import Path
@@ -215,6 +216,7 @@ class DerivativeEvaluator(object):
 		central_difference_coefficients_dictionary['21'] = {'factors':[0.0, -2.0, 2.0, -1.0, 1.0, -1.0, 1.0], 'perturbations_list': [[0.0, -1.0], [0.0, 1.0], [-1.0, -1.0], [-1.0, 1.0], [1.0, -1.0], [1.0, 1.0]]}
 		central_difference_coefficients_dictionary['12'] = {'factors':[0.0, -2.0, 2.0, -1.0, 1.0, -1.0, 1.0], 'perturbations_list': [[-1.0, 0.0], [1.0, 0.0], [-1.0, -1.0], [1.0, -1.0], [-1.0, 1.0], [1.0, 1.0]]}
 		central_difference_coefficients_dictionary['111'] = {'factors':[0.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0], 'perturbations_list': [[-1.0, -1.0, -1.0], [-1.0, -1.0, 1.0], [-1.0, 1.0, -1.0], [-1.0, 1.0, 1.0], [1.0, -1.0, -1.0], [1.0, -1.0, 1.0], [1.0, 1.0, -1.0], [1.0, 1.0, 1.0]]}		
+		#central_difference_coefficients_dictionary['3'] =  {'factors':[6.0, 1.0, -4.0, -4.0, 1.0], 'perturbations_list': [[-1.0], [-0.5], [0.5], [1.0]]}
 		central_difference_coefficients_dictionary['4'] =  {'factors':[6.0, 1.0, -4.0, -4.0, 1.0], 'perturbations_list': [[-1.0], [-0.5], [0.5], [1.0]]}
 
 		return central_difference_coefficients_dictionary
