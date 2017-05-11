41a128347afb3224389449d5f8d4e65ed1423c8d
Thomas Angsten <tangst10@gmail.com>
2017-05-11 14:32:39 -0700

    Trivial debug change

--- fpctoolkit/structure_prediction/taylor_expansion/minima_relaxer.py
+++ fpctoolkit/structure_prediction/taylor_expansion/minima_relaxer.py
@@ -6,6 +6,7 @@ import numpy as np
 from fpctoolkit.workflow.vasp_static_run_set import VaspStaticRunSet
 from fpctoolkit.util.path import Path
 from fpctoolkit.phonon.eigen_structure import EigenStructure
+from fpctoolkit.workflow.vasp_relaxation import VaspRelaxation
 
 
 
@@ -16,20 +17,23 @@ class MinimaRelaxer(object):
 	The relaxations are then sortable by energy, and one can see the change in the eigen_chromosomes in going from the guess to the relaxed structure
 	"""
 
-	def __init__(self, path, reference_structure, hessian, taylor_expansion, reference_completed_vasp_relaxation_run, vasp_run_inputs_dictionary, perturbation_magnitudes_dictionary):
+	def __init__(self, path, reference_structure, hessian, vasp_relaxation_inputs_dictionary, eigen_chromosome_energy_pairs_list):
 		"""
-		taylor_expansion should be an initialized TaylorExpansion instance with the appropriate terms.
-		
-		perturbation_magnitudes_dictionary should look like {'strain': 0.02, 'displacement': 0.01} with strain as fractional and displacement in angstroms
+		eigen_chromosome_energy_pairs_list should look like [[guessed eigen_chromosome, predicted energy], [...],...]
 
-		vasp_run_inputs_dictionary should look like:
+		vasp_relaxation_inputs_dictionary should look like:
 
-		vasp_run_inputs_dictionary = {
-			'kpoint_scheme': 'Monkhorst',
-			'kpoint_subdivisions_list': [4, 4, 4],
-			'encut': 800,
-			'npar': 1 (optional),
-			other incar params
+		vasp_relaxation_inputs_dictionary = 
+		{
+			'external_relaxation_count': 4,
+			'kpoint_schemes_list': ['Gamma'],
+			'kpoint_subdivisions_lists': [[1, 1, 1], [1, 1, 2], [2, 2, 4]],
+			'submission_script_modification_keys_list': ['100', 'standard', 'standard_gamma'], #optional - will default to whatever queue adapter gives
+			'submission_node_count_list': [1, 2],
+			'ediff': [0.001, 0.00001, 0.0000001],
+			'encut': [200, 400, 600, 800],
+			'isif' : [5, 2, 3]
+			#any other incar parameters with value as a list
 		}
 		"""
 
@@ -37,18 +41,63 @@ class MinimaRelaxer(object):
 		self.reference_structure = reference_structure
 		self.hessian = hessian
 		self.eigen_pairs_list = hessian.get_sorted_hessian_eigen_pairs_list()
-		self.taylor_expansion = copy.deepcopy(taylor_expansion)
-		self.vasp_run_inputs_dictionary = copy.deepcopy(vasp_run_inputs_dictionary)
-		self.perturbation_magnitudes_dictionary = perturbation_magnitudes_dictionary
+		self.vasp_relaxation_inputs_dictionary = copy.deepcopy(vasp_relaxation_inputs_dictionary)
 
-		if not reference_completed_vasp_relaxation_run.complete:
-			raise Exception("Vasp relaxation for reference structure is not yet completed")
-
-		self.reference_completed_vasp_relaxation_run = reference_completed_vasp_relaxation_run
+		self.eigen_chromosomes_list = [eigen_chromosome_energy_pair[0] for eigen_chromosome_energy_pair in eigen_chromosome_energy_pairs_list]
+		self.predicted_energies_list = [eigen_chromosome_energy_pair[1] for eigen_chromosome_energy_pair in eigen_chromosome_energy_pairs_list]
 
 
+		self.completed_relaxations_data_list = [] #list of lists with each component like [relaxation, initial chromosome, final chromosome]
 
-		self.vasp_static_run_sets_list = None
+		self.vasp_relaxations_list = None
+
+		self.initialize_relaxation_list()
+
+
+
+	def initialize_relaxation_list(self):
+
+		self.vasp_relaxations_list = []
+
+		for i, eigen_chromosome in enumerate(self.eigen_chromosomes_list):
+			eigen_structure = EigenStructure(reference_structure=self.reference_structure, hessian=self.hessian)
+			eigen_structure.set_eigen_chromosome(eigen_chromosome)
+
+			initial_structure = eigen_structure.get_distorted_structure()
+
+			self.vasp_relaxations_list.append(VaspRelaxation(path='structure_'+str(i), initial_structure=initial_structure, input_dictionary=self.vasp_relaxation_inputs_dictionary))
 
 
 	def update(self):
+
+		self.completed_relaxations_data_list = []
+
+		for i, vasp_relaxation in enumerate(self.vasp_relaxations_list):
+			vasp_relaxation.update()
+
+			if vasp_relaxation.complete:
+				eigen_structure = EigenStructure(reference_structure=self.reference_structure, hessian=self.hessian, distorted_structure=vasp_relaxation.final_structure)
+
+				self.completed_relaxations_data_list.append([vasp_relaxation, sef.eigen_chromosomes_list[i], eigen_structure.get_list_represenation()])
+
+				print "Structure", str(i)
+				print "Predicted Energy Change", str(self.predicted_energies_list[i])
+				print "Original Chromosome"
+				print str(self.eigen_chromosomes_list[i])
+				print "Final Chromosome"
+				print str(eigen_structure.get_list_represenation())
+
+
+	def get_sorted_relaxation_data_list(self):
+		"""
+		Returns a list of lists with each component like [relaxation, initial chromosome, final chromosome] sorted by relaxation energy (lowest to highest)
+		"""
+
+
+	def complete(self):
+
+		for vasp_relaxation in self.vasp_relaxations_list:
+			if not vasp_relaxation.complete:
+				return False
+
+		return True
