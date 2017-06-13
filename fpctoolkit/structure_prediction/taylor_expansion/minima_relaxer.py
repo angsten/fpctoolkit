56f0b162ec71df25bcc8e4f6066b4ca92c2b1b89
Thomas Angsten <tangst10@gmail.com>
2017-06-13 12:58:57 -0700

    Trivial debug change

--- fpctoolkit/structure_prediction/taylor_expansion/minima_relaxer.py
+++ fpctoolkit/structure_prediction/taylor_expansion/minima_relaxer.py
@@ -97,6 +97,7 @@ class MinimaRelaxer(object):
 		final_pairs_list = []
 		energies_list = []
 		seen_before_dictionary = {}
+		print 'analyzing unique pairs in minima relax'
 		for line in full_guesses_list_file:
 			energy = float(su.remove_extra_spaces(line.split('[')[0]))
 			chromosome = [float(x) for x in su.remove_extra_spaces(line[line.find('[')+1:line.find(']')]).split(' ')]
