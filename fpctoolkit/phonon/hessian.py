7856b102f4792bbbfaafa97cc29771671a8f0e62
Thomas Angsten <tangst10@gmail.com>
2017-06-15 12:31:51 -0700

    Trivial debug change

--- fpctoolkit/phonon/hessian.py
+++ fpctoolkit/phonon/hessian.py
@@ -143,8 +143,8 @@ class Hessian(object):
 			if eigen_pair.is_translational_mode():
 				translational_count += 1
 
-		if not translational_count == 3:
-			raise Exception("Number of translational modes in eigen list must be exactly three.", translational_count)
+		# if not translational_count == 3:
+		# 	raise Exception("Number of translational modes in eigen list must be exactly three.", translational_count)
 
 	@staticmethod
 	def validate_eigen_pairs_are_orthonormal(eigen_pairs_list):
