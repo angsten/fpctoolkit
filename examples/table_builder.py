6cb6b3b7a4011c471d8f051cdd4d23379b685808
Thomas Angsten <tangst10@gmail.com>
2017-06-20 12:20:03 -0700

    Trivial debug change

--- examples/table_builder.py
+++ examples/table_builder.py
@@ -44,7 +44,7 @@ def get_table_chunk(misfit_strain, mode_count):
 			polarizations_list.append('*')
 
  	output_string += "& $\lambda_i$ &" + " & ".join(eigen_values_list) + '\n'
- 	output_string += str(misfit_strain) + "&  $\vec{Z}_i$   &" + " & ".join(polarizations_list)
+ 	output_string += str(misfit_strain) + "&  $\\vec{Z}_i$   &" + " & ".join(polarizations_list)
 
  	return output_string
 
