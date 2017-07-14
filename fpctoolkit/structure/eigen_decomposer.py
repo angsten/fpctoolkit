37942c9aa44a87b14d2855dd29d8725a53b10dcd
Thomas Angsten <tangst10@gmail.com>
2017-07-14 10:54:04 -0700

    Trivial debug change

--- fpctoolkit/structure/eigen_decomposer.py
+++ fpctoolkit/structure/eigen_decomposer.py
@@ -580,7 +580,7 @@ def get_displacement_second_derivative(path, reference_structure, eigen_index, v
 		force_sums_list = [0.0, force_sum, -1.0*force_sum] ################assumes centrosymmetry and one element in force sumes list
 
 		numerator = sum(map(lambda x, y: -x*y, term_factors_list, force_sums_list))
-		denominator = 2.0*displacement_factor
+		denominator = 2.0*displacement_magnitude_factor
 
 		return numerator/denominator
 
