135564ab569f9ee64707579cc5269d45ea3a1d4e
Thomas Angsten <tangst10@gmail.com>
2017-08-28 12:00:44 -0700

    Trivial debug change

--- fpctoolkit/structure_prediction/taylor_expansion/derivative_evaluator.py
+++ fpctoolkit/structure_prediction/taylor_expansion/derivative_evaluator.py
@@ -381,7 +381,7 @@ class DerivativeEvaluator(object):
 
 		distorted_structure = vasp_static_run_set.get_final_structures_list()[0]
 
-		 distorted_structure.lattice = self.reference_structure.lattice
+		distorted_structure.lattice = self.reference_structure.lattice
 
 		basis_vector = DisplacementVector.get_instance_from_displaced_structure_relative_to_reference_structure(reference_structure=self.reference_structure, 
 			displaced_structure=distorted_structure, coordinate_mode='Cartesian')
