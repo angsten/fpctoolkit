b8490009c14cdc13c0d82b8e4d307d60dafce332
Thomas Angsten <tangst10@gmail.com>
2017-09-06 12:14:34 -0700

    Trivial debug change

--- fpctoolkit/util/path.py
+++ fpctoolkit/util/path.py
@@ -35,14 +35,11 @@ class Path(object):
 
 		components_list = Path.split_into_components(path)
 
-		sub_path = components_list[0]
+		sub_path = '/'
 
 		print "compon list " + str(components_list)
 
 		for i, component in enumerate(components_list):
-
-			if i == 0:
-				continue
 
 			print "comp is " + str(component)
 			sub_path = Path.join(sub_path, component)
