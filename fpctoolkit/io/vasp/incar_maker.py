3c1185ead1098e602ecdb417c18f72fef709c370
Thomas Angsten <tangst10@gmail.com>
2017-09-08 18:10:20 -0700

    Trivial debug change

--- fpctoolkit/io/vasp/incar_maker.py
+++ fpctoolkit/io/vasp/incar_maker.py
@@ -24,6 +24,7 @@ class IncarMaker(object):
 		incar['prec'] = 'Accurate'
 		incar['ediff'] = 1.0e-6
 		incar['encut'] = 600
+		incar['lasph'] = True
 		# incar['lwave'] = False
 		# incar['lcharg'] = False
 
@@ -46,6 +47,7 @@ class IncarMaker(object):
 		incar['ediff'] = 1.0e-6
 		incar['encut'] = 600
 		incar['lwave'] = True
+		incar['lasph'] = True
 		# incar['lcharg'] = False
 
 		incar.modify_from_dictionary(custom_parameters_dictionary)
