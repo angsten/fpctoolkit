98524d8b4ae4b59467a7627e27f2fd0f225ff6c1
Thomas Angsten <tangst10@gmail.com>
2017-12-06 12:39:41 -0800

    Trivial debug change

--- fpctoolkit/util/math/tensor.py
+++ fpctoolkit/util/math/tensor.py
@@ -30,3 +30,25 @@ class Tensor(object):
 		full_tensor.append([voigt_tensor[4]/2.0, voigt_tensor[3]/2.0, voigt_tensor[2]])
 
 		return np.array(full_tensor)
+
+
+	@staticmethod
+	def convert_voigt_to_3x3_tensor(voigt_tensor):
+		"""
+		"""
+
+		voigt_tensor = np.array(voigt_tensor)
+
+		if not voigt_tensor.ndim == 1:
+			raise Exception("Number of array dimensions of voigt tensor must be 1. Input tensor: " + str(voigt_tensor))
+
+		if not len(voigt_tensor) == 6:
+			raise Exception("Voigt tensor must have six components. Input tensor: " + str(voigt_tensor))
+
+		full_tensor = []
+
+		full_tensor.append([voigt_tensor[0], voigt_tensor[5], voigt_tensor[4]])
+		full_tensor.append([voigt_tensor[5], voigt_tensor[1], voigt_tensor[3]])
+		full_tensor.append([voigt_tensor[4], voigt_tensor[3], voigt_tensor[2]])
+
+		return np.array(full_tensor)
